72916f874fa6e1c2360c2182262c93b19046ac61
YousufSiddiqui <siddiquy@student.hu-berlin.de>
2016-06-22 16:47:51 +0200

    Included normal density distribution on Histograms

--- QuantitativeHist/Histograms.R
+++ QuantitativeHist/Histograms.R
@@ -20,60 +20,91 @@ colnames(C_D) = c("Account_Status","Months","Credit_History","Credit_Purpose","C
 #####Histograms######
 graphics.off()
 
-library(formatR)
-
 
 par(mfrow=c(3,4))
 
 #Variable: Months
 
-h1 = hist(C_D$Months,col = "blue",breaks = 10,ylim = c(0,300),
-          xlim = c(min(C_D$Months),max(C_D$Months)),xlab = "No.of Months", 
+g1 = C_D$Months
+m1 = mean(g1)
+std1 = sqrt(var(g1))
+h1 = hist(g1,col ="gray",breaks = 10, prob=TRUE,
+          ylim = c(0,0.05),xlim = c(min(C_D$Months),max(C_D$Months)),xlab = "No.of Months", 
           main = "Histogram of Variable: Months")
+c1 = curve(dnorm(x, mean=m1, sd=std1), 
+      col="red", lwd=2, add=TRUE, yaxt="n")
+
 
 #Variable:Age
 
 
-
-
-h2 = hist(C_D$Age,col = "blue",breaks = 20,ylim = c(0,150),
-          xlim = c(min(C_D$Age),max(C_D$Age)),xlab = "Age in Years", 
+g2 = C_D$Age
+m2 = mean(g2)
+std2 = sqrt(var(g2))
+h2 = hist(g2,col = "gray",breaks = 20, prob= TRUE,
+          ylim = c(0,0.05),xlim = c(min(C_D$Age),max(C_D$Age)),xlab = "Age in Years", 
           main = "Histogram of Variable: Age")
-
+c2 = curve(dnorm(x, mean=m2, sd=std2), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 #Variable:Credit Amount
 
-
-h3 = hist(C_D$Credit_Amount,col = "blue",breaks = 50,ylim = c(0,220),
-          xlim = c(min(C_D$Credit_Amount),max(C_D$Credit_Amount)),xlab = "Credit Amount", 
+g3 = C_D$Credit_Amount
+m3 = mean(g3)
+std3 = sqrt(var(g3))
+h3 = hist(g3,col = "gray",breaks = 20, prob= TRUE,
+          ylim = c(0,0.00035),xlim = c(min(C_D$Credit_Amount),max(C_D$Credit_Amount)),xlab = "Credit Amount", 
           main = "Histogram of Variable: Credit_Amount")
+c3 = curve(dnorm(x, mean=m3, sd=std3), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 
 #Variable: Residence Since 
 
-h4 = hist(C_D$Residence_Since,col = "blue",breaks = 5,ylim = c(0,220),
-          xlim = c(min(C_D$Residence_Since),max(C_D$Residence_Since)),xlab = "Residence_Since", 
+g4 = C_D$Residence_Since
+m4 = mean(g4)
+std4 = sqrt(var(g4))
+h4 = hist(g4,col = "gray",breaks = 5,prob= TRUE,
+          ylim = c(0,0.7),xlim = c(min(C_D$Residence_Since),max(C_D$Residence_Since)),xlab = "Residence_Since", 
           main = "Histogram of Variable: Residence_Since")
+c4 = curve(dnorm(x, mean=m4, sd=std4), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 
-#lines(density(C_D$Residence_Since, adjust = 220), col = "orange")
+
 
 #Variable: Installment Rate
 
-h5 = hist(C_D$Installment_Rate,col = "blue",breaks = 5,ylim = c(0,600),
+
+g5 = C_D$Installment_Rate
+m5 = mean(g5)
+std5 = sqrt(var(g5))
+h5 = hist(g5,col = "gray",breaks = 5,prob= TRUE,ylim = c(0,0.5),
           xlim = c(min(C_D$Installment_Rate),max(C_D$Installment_Rate)),xlab = "Installment Rate", 
           main = "Histogram of Variable: Installment Rate")
+c5 = curve(dnorm(x, mean=m5, sd=std5), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 
 #Variable: Existing Credit
 
 
-h6 = hist(C_D$Existing_Credit,col = "blue",breaks = 5,ylim = c(0,700),
+g6 = C_D$Existing_Credit
+m6 = mean(g6)
+std6 = sqrt(var(g6))
+h6 = hist(g6,col = "gray",breaks = 5,prob= TRUE,ylim = c(0,1.5),
           xlim = c(min(C_D$Existing_Credit),max(C_D$Existing_Credit)),xlab = "Existing Credit", 
           main = "Histogram of Variable: Existing Credit")
+c6 = curve(dnorm(x, mean=m6, sd=std6), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 
 
 #Variable: Dependents
 
-h7 = hist(C_D$Dependents,col = "blue",breaks = 4,ylim = c(0,1000),
+
+g7 = C_D$Dependents
+m7 = mean(g7)
+std7 = sqrt(var(g7))
+h7 = hist(g7,col = "gray",breaks = 4,prob= TRUE,ylim = c(0,4.5),
           xlim = c(min(C_D$Dependents),max(C_D$Dependents)),xlab = "Dependents", 
           main = "Histogram of Variable: Dependents")
-
+c7 = curve(dnorm(x, mean=m7, sd=std7), 
+           col="red", lwd=2, add=TRUE, yaxt="n")
 
 
